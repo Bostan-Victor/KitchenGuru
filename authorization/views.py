0dd2d1f52df531aec4ad9a894b3ad54cebaaaf67
Bostan Victor <victorbostan01@gmail.com>
2023-09-16 20:18:58 +0300

    Created Change Password logic

--- authorization/views.py
+++ authorization/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.hashers import check_password
 from users import models
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth.hashers import make_password
 
 
 class RegisterView(generics.CreateAPIView):
@@ -37,46 +36,26 @@ def login_view(request):
     return Response({"message": "Password invalid!"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['PUT'])
-# @permission_classes([IsAuthenticated])
-# def change_password_view(request):
-#     data = request.data
-#     user = request.user
-    
-#     if check_password(data['password'], user.password):
-#         serializer = serializers.ChangePasswordSerializer(user, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     return Response({"message": "Old password invalid!"}, status=status.HTTP_400_BAD_REQUEST)
-
-
 class ChangePasswordView(generics.UpdateAPIView):
     serializer_class = serializers.ChangePasswordSerializer
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user
-
-    def validate(self, request):
-        data = request.data
-        user = self.get_object()
-        if check_password(data['password'], user.password):
-            if len(data['password']) < 8:
-                raise serializers.ValidationError("Password is shorter than 8 characters!")
-            if data['new_password'] != data['confirm_new_password']:
-                raise serializers.ValidationError("New password fields do not match!")
-        else:
-            raise serializers.ValidationError("Old Password is incorrect!")
-        return data
     
     def update(self, request, *args, **kwargs):
         data = request.data
         user = self.get_object()
 
-        new_password = data['new_password']
-        user.set_password(new_password)
-        user.save()
+        if check_password(data['password'], user.password):
+            if len(data['new_password']) < 8:
+                return Response({"message": "Password shorter than 8 characters"}, status=status.HTTP_400_BAD_REQUEST)
+            if data['new_password'] != data['confirm_new_password']:
+                return Response({"message": "New password fields do not match!"}, status=status.HTTP_400_BAD_REQUEST)
+            new_password = data['new_password']
+            user.set_password(new_password)
+            user.save()
 
-        return Response({"message": "Password updated succesfully!"}, status=status.HTTP_200_OK)
+            return Response({"message": "Password updated succesfully!"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Old password is incorrect!"}, status=status.HTTP_400_BAD_REQUEST)
