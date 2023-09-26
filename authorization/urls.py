0e8ebb1bdaf5f349e1e2a613cb9ed86de01155da
Bostan Victor <victorbostan01@gmail.com>
2023-09-26 21:26:33 +0300

    Add Password Recovery

--- authorization/urls.py
+++ authorization/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("login", views.login_view),
     path("change-password/", views.ChangePasswordView.as_view()),
     path("token", TokenObtainPairView.as_view(), name= 'token_obtain_pair'),
-    path("token/refresh", TokenRefreshView.as_view(), name='token_refresh')
+    path("token/refresh", TokenRefreshView.as_view(), name='token_refresh'),
+    path('password-recovery-request', views.password_recovery_request_view),
+    path('password-recovery-confirm', views.password_recovery_confirm_view),
+    path('password-recovery-change', views.PasswordRecoveryChangeView.as_view())
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -6,7 +6,9 @@ from django.contrib.auth.hashers import check_password
 from users import models
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from datetime import datetime
+from django.core.mail import send_mail
+from datetime import datetime, timedelta
+import random
 
 
 class RegisterView(generics.CreateAPIView):
@@ -65,3 +67,72 @@ class ChangePasswordView(generics.UpdateAPIView):
             return Response({"message": "Password updated succesfully!"}, status=status.HTTP_200_OK)
         else:
             return Response({"message": "Old password is incorrect!"}, status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['POST'])
+def password_recovery_request_view(request):
+    data = request.data
+    try:
+        user = models.Users.objects.get(email=data['email'])
+    except:
+        return Response({'message': 'There is no account created with this email!'}, status=status.HTTP_404_NOT_FOUND)
+    pass_reset_code = models.PasswordResetCode.objects.get(user_id=user.id)
+    code = str(random.randint(100000, 999999))
+    pass_reset_code.code = code
+    pass_reset_code.created_at = datetime.now()
+    pass_reset_code.save()
+    send_mail(
+        'KitchenGuru - Password reset code',
+        f'Your 6-digit code:\n{code}',
+        'KitchenGuru@mail.com',
+        [data['email']],
+        fail_silently=False
+    )
+    return Response({'message': 'Email sent succesfully!'}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def password_recovery_confirm_view(request):
+    data = request.data
+    delta = timedelta(minutes=5)
+
+    if len(data['code']) > 6:
+        return Response({'message': 'The code is not the right length!'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        user = models.Users.objects.get(email=data['email'])
+        pass_reset_object = models.PasswordResetCode.objects.get(user_id=user.id)
+        created_at = pass_reset_object.created_at.replace(tzinfo=None)
+        now = datetime.now()
+
+        if created_at - now > delta:
+            return Response({'message': 'The code has expired!'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if data['code'] != pass_reset_object.code:
+                return Response({'messasge': 'The code is entered incorrectly!'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                refresh = RefreshToken.for_user(user)
+                access_token =  str(refresh.access_token)
+                return Response({
+                    'message': 'Code entered correctly!',
+                    "access_token": access_token,
+                    "refresh_token": str(refresh)
+                }, status=status.HTTP_200_OK)
+            
+
+class PasswordRecoveryChangeView(generics.UpdateAPIView):
+    serializer_class = serializers.PasswordRecoveryChange
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        data = request.data
+        user = request.user
+
+        if len(data['new_password']) < 8:
+            return Response({"message": "Password shorter than 8 characters"}, status=status.HTTP_400_BAD_REQUEST)
+        if data['new_password'] != data['confirm_new_password']:
+            return Response({"message": "New password fields do not match!"}, status=status.HTTP_400_BAD_REQUEST)
+        new_password = data['new_password']
+        user.set_password(new_password)
+        user.save()
+        profile = models.Profiles.objects.get(user_id=user.id)
+        profile.updated = datetime.now()
+        profile.save()
+        return Response({"message": "Password updated succesfully!"}, status=status.HTTP_200_OK)
