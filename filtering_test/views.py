2bf56bd1c0814272556d057e6f24ab19e7a7366d
Renat03 <grsrenat@gmail.com>
2023-10-04 10:58:21 +0300

    Modified filtering feature

--- filtering_test/views.py
+++ filtering_test/views.py
@@ -1,32 +1,46 @@
 from rest_framework import generics, status
 from filtering_test import serializers, models
-from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from django.db.models import Max
 
 
-class FilteringView(generics.ListAPIView):    
+class FilteringView(generics.ListAPIView):
+    serializer_class = serializers.FilteringSerializer
+    ordering_fields = ['comment_count', 'rating', 'favorites_count']
+    ordering = ['rating']
+
     def list(self, request):
         queryset = models.TestRecipes.objects.all()
+        db_max_duration = models.TestRecipes.objects.aggregate(db_max_duration=Max('duration_time'))['db_max_duration']
 
-        data = request.data
-        sort_by = data.get('sort_by')
-        category = data.get('category')
-        duration_min = data.get('duration_min')
-        duration_max = data.get('duration_max')
-
-        if sort_by:
-            queryset = queryset.order_by(sort_by)
+        sort_by = self.request.query_params.get('sort_by')
+        categories = self.request.query_params.get('category')
         
-        if category:
-            queryset = queryset.filter(category__in=category)
+        try:
+            duration_min = int(self.request.query_params.get('duration_min', 0))
+            duration_max = int(self.request.query_params.get('duration_max', db_max_duration))
+        except ValueError:
+            return Response({"message": "Duration should be a valid number."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if not (0 <= duration_min <= duration_max <= db_max_duration):
+            return Response({"message": "Invalid duration parameters."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        queryset = queryset.filter(duration_time__gte=duration_min, duration_time__lte=duration_max)
+        
+        if categories:
+            categories_list = categories.split(',')
+            queryset = queryset.filter(category__in=categories_list)
+        
+        order_by_field = sort_by if sort_by in self.ordering_fields else self.ordering[0]
+        queryset = queryset.order_by(order_by_field)
 
-        if duration_min:
-            queryset = queryset.filter(duration_time__gte=duration_min)
+        if not queryset.exists():
+            return Response({"message": "There are no recipes that match these filters"}, status=status.HTTP_404_NOT_FOUND)
+        
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
-        if duration_max:
-            queryset = queryset.filter(duration_time__lte=duration_max)
-
-
-        serializer = serializers.FilteringSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
