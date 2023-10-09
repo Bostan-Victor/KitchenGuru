0c460d46d986e1c7ff0a1188f995fec5b4e74988
Bostan Victor <victorbostan01@gmail.com>
2023-10-09 17:49:21 +0300

    Delete all the things.

--- recipes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from recipes import views
-
-
-urlpatterns = [
-    path('create-recipe', views.CreateRecipeView.as_view()),
-    path('recipe-review', views.CreateReviewView.as_view())
-    # path('get-recipes', views.GetRecipes.as_view())
-]
